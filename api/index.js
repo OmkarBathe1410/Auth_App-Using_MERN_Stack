f1a46c4f17ef1405864ba9bc96557d182adc775c
Omkar Satish Bathe <omi.bathe@gmail.com>
2023-10-05 20:53:36 +0530

    Create SignUp API Route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 // API Routes:
 app.listen(3000, () => {
   console.log('Server Listening on port 3000');
@@ -20,3 +23,4 @@ app.listen(3000, () => {
 
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
